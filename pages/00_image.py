289c368bbae4c4f3a177fdb16f5ff5d3effe5963
jwentertainer <155875238+jwentertainer@users.noreply.github.com>
2025-06-21 13:13:51 +0900

    Update 00_image.py

--- pages/00_image.py
+++ pages/00_image.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.image(https://www.heritage.go.kr/unisearch/images/imp_intangible_cult_prop/thumb/2021071309135601.jpg)
+st.image("https://www.heritage.go.kr/unisearch/images/imp_intangible_cult_prop/thumb/2021071309135601.jpg")
